b8b29ce3e8f446ae709b5d11210ee7a5518f2961
Massyl <massil.nait@gmail.com>
2016-09-09 23:05:57 +0200

    enhancing type representation

--- src/Dynamic.hs
+++ src/Dynamic.hs
@@ -1,52 +1,73 @@
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs #-}
 
 module Dynamic where
 
-import Leibniz (Equal, Equal(..), subst, refl, sym, trans)
+import Leibniz (Equal, Equal(..), subst, refl, sym, trans, substitute,congruence)
 import Data.Functor.Identity
-import Data.Functor(fmap)
+import Control.Applicative(pure, (<$>), (<*>))
 
 data Dynamic rep where {
   (:::) :: a -> rep a -> Dynamic rep
 }
 
-class TypeRep rep where {
+class MaybeLeibniz rep where {
   (<=>) :: rep a -> rep  b -> Maybe (Equal a b)
  }
 
-data TpCon a = Int (Equal a Int) | Bool ( Equal a Bool)
+data TypeRepConst a = Int (Equal a Int) | Bool ( Equal a Bool)
+data TypeRep tpr a  = TypeConst (tpr a)
+                 | forall x. List (Equal a [x])(TypeRep tpr x)
+                 | forall x y. Func (Equal a (x -> y)) (TypeRep tpr x) (TypeRep tpr y)
+
+type Type = TypeRep TypeRepConst                   
 
 coerce :: Equal a b
        -> a
        -> b
 coerce = subst Identity runIdentity
 
-fromDynamic :: TypeRep rep  => rep a
+fromDynamic :: MaybeLeibniz rep  => rep a
             -> Dynamic rep
             -> Maybe a
 fromDynamic expected (a ::: actual) = case actual <=> expected of
                                 Just eq -> return $ coerce eq a
                                 _ -> Nothing
 
-fromDynamic2 :: TypeRep tpr => tpr a
+fromDynamic2 :: MaybeLeibniz tpr => tpr a
              -> Dynamic tpr
              -> Maybe a
 fromDynamic2 expected (a ::: actual) = fmap (`coerce` a) $ actual <=> expected
 
-instance TypeRep TpCon where
+instance MaybeLeibniz TypeRepConst where
   (Int p1)  <=> (Int p2)  = return $ trans p1 $ sym p2
   (Bool p1) <=> (Bool p2) = return $ trans p1 $ sym p2
   _ <=> _ = Nothing
 
+instance MaybeLeibniz tpr => MaybeLeibniz (TypeRep tpr) where
+  TypeConst x <=> TypeConst y = x <=> y
+  List eq1 rep1 <=> List eq2 rep2 = trans eq1 . sym . (`substitute` eq2) <$> rep2 <=> rep1
+  Func eqf repArg1 repRes1 <=> Func eqg repArg2 repRes2 = trans eqf . (`trans` sym eqg) <$> (congruence <$> repArg1<=>repArg2 <*> repRes1<=>repRes2)
+    
+intTypeRepConst :: TypeRepConst Int
+intTypeRepConst = Int refl
 
-intRep :: TpCon Int
-intRep = Int refl
+boolTypeRepConst :: TypeRepConst Bool
+boolTypeRepConst = Bool refl
 
-boolRep :: TpCon Bool
-boolRep = Bool refl
+ten :: Dynamic TypeRepConst
+ten = 10 ::: intTypeRepConst
 
-ten :: Dynamic TpCon
-ten = 10 ::: intRep
+true :: Dynamic TypeRepConst
+true = True ::: boolTypeRepConst
 
-true :: Dynamic TpCon
-true = True ::: boolRep
+intTypeRep :: Type Int
+intTypeRep = TypeConst intTypeRepConst
+
+boolTypeRep:: Type Bool
+boolTypeRep = TypeConst boolTypeRepConst
+
+listRep :: TypeRep tpr a -> TypeRep tpr [a]
+listRep tpa = List refl tpa
+
+-- (.->.)                          
