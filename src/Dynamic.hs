c9d0996b3383837c59e943b924ccfd9cd62b570a
Massyl <massil.nait@gmail.com>
2016-09-15 01:19:58 +0200

    some code refactoring

--- src/Dynamic.hs
+++ src/Dynamic.hs
@@ -30,8 +30,8 @@ import           Control.Applicative   ((<$>), (<*>))
 import           Data.Functor          (fmap)
 import           Data.Functor.Identity
 import           Data.Maybe            (fromJust)
-import           Leibniz               (Equal, deduce, refl, subst, substitute,
-                                        sym, trans)
+import           Leibniz               (Equal, deduce, refl, subst, substitute,sym, trans)
+import           Control.Monad (join)
 
 data Dynamic rep where {
   (:::) :: a -> rep a -> Dynamic rep
@@ -41,12 +41,11 @@ class Comparable rep where {
   (<=>) :: rep a -> rep  b -> Maybe (Equal a b)
  }
 
-data TypeRepConst a = Int (Equal a Int) | Bool ( Equal a Bool)
+data TypeRepConst a = Int (Equal a Int) | Bool ( Equal a Bool)| String (Equal a String)
 data TypeRep tpr a  = TypeConst (tpr a)
                  | forall x. List (Equal a [x])(TypeRep tpr x)
                  | forall x y. Func (Equal a (x -> y)) (TypeRep tpr x) (TypeRep tpr y)
 
-type Type = TypeRep TypeRepConst
 coerce :: Equal a b
        -> a
        -> b
@@ -61,6 +60,7 @@ fromDynamic expected (a ::: actual) = (`coerce` a) <$> actual <=> expected
 instance Comparable TypeRepConst where
   Int p1  <=> Int p2  = return $ trans p1 $ sym p2
   Bool p1 <=> Bool p2 = return $ trans p1 $ sym p2
+  String p1 <=> String p2 = return $ trans p1 $ sym p2
   _ <=> _ = Nothing
 
 instance Comparable tpr => Comparable (TypeRep tpr) where
@@ -73,43 +73,35 @@ infixr 5 .->
 (.->) :: TypeRep tpr a -> TypeRep tpr b -> TypeRep tpr (a -> b)
 tpra .-> tprb = Func refl tpra tprb
 
-intTypeRepConst :: TypeRepConst Int
-intTypeRepConst = Int refl
+intRep :: TypeRep TypeRepConst Int
+intRep = TypeConst $ Int refl 
 
-boolTypeRepConst :: TypeRepConst Bool
-boolTypeRepConst = Bool refl
-
-ten :: Dynamic TypeRepConst
-ten = 10 ::: intTypeRepConst
-
-true :: Dynamic TypeRepConst
-true = True ::: boolTypeRepConst
-
-intTypeRep :: Type Int
-intTypeRep = TypeConst intTypeRepConst
-
-boolTypeRep:: Type Bool
-boolTypeRep = TypeConst boolTypeRepConst
+boolTypeRep:: TypeRep TypeRepConst Bool
+boolTypeRep = TypeConst $ Bool refl
 
 listRep :: TypeRep tpr a -> TypeRep tpr [a]
 listRep tpa = List refl tpa
 
 intsToBool :: TypeRep TypeRepConst ([Int] -> Bool)
-intsToBool = listRep intTypeRep .-> boolTypeRep
+intsToBool = listRep intRep .-> boolTypeRep
 
 plusRep :: TypeRep TypeRepConst (Int -> Int -> Int)
-plusRep = (intTypeRep .-> intTypeRep .-> intTypeRep)
-plus :: Dynamic Type
+plusRep = (intRep .-> intRep .-> intRep)
+
+plus :: Dynamic (TypeRep TypeRepConst)
 plus = (+) ::: plusRep
 
 one :: Dynamic (TypeRep TypeRepConst)
-one = 1 ::: intTypeRep
+one = 1 ::: intRep
+
+true :: Dynamic (TypeRep TypeRepConst)
+true = True ::: boolTypeRep
 
 twelve :: Dynamic (TypeRep TypeRepConst)
-twelve = 12 ::: intTypeRep
+twelve = 12 ::: intRep
 
 thirteen :: Int
-thirteen = fromJust $ fromDynamic plusRep plus <*> fromDynamic intTypeRep one <*> fromDynamic intTypeRep twelve
+thirteen = fromJust $ plus `dynApply` one >>= (`dynApply` twelve) >>= fromDynamic intRep
 
 dynApply :: Comparable tpr => Dynamic (TypeRep tpr)
          -> Dynamic (TypeRep tpr)
@@ -118,8 +110,8 @@ dynApply (f:::frep) (x:::xrep)= case frep of
                                 Func eqf arg res  -> (:::res) . coerce eqf f . (`coerce` x) <$> xrep<=>arg
                                 _ -> Nothing
 
-increment :: Dynamic Type
+increment :: Dynamic (TypeRep TypeRepConst)
 increment = fromJust $ dynApply plus one
 
 successor :: Int -> Int
-successor = fromJust $ fromDynamic (intTypeRep .-> intTypeRep) increment
+successor = fromJust $ fromDynamic (intRep .-> intRep) increment
